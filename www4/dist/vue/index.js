dd0db668c971f2ab97d1828155f369e1326ef88e
qi.ying <qi.ying@cesgroup.com.cn>
2017-03-16 19:25:46 +0800

    基础

--- www4/dist/vue/index.js
+++ www4/dist/vue/index.js
@@ -7,10 +7,11 @@
 var storage = window.localStorage;
 // 初始化本地存储内容
 
-// 1. 定义（路由）组件。
-// 可以从其他文件 import 进来
-var MainPage = {
-    template: '#mainpage',
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+var app = new Vue({
+	el: '.page-tabbar',
     data: function () {
         return {
             selected: '基础产品',
@@ -23,7 +24,7 @@ var MainPage = {
             yjshtml: '',
             datas: null,
             groups: [],
-            wlwDatas: null,
+            wlwDatas: null
         }
     },
     watch: {
@@ -90,7 +91,7 @@ var MainPage = {
                         },
                         "query" : {
                             "startTime": "",
-                            "endTime": "",
+                            "endTime": ""
                         }
                     };
                 }
@@ -148,11 +149,20 @@ var MainPage = {
 
     },
     beforeCreate: function() {
-        this.$options.methods.loadYjsData.bind(this)();
-        this.$options.methods.loadWlwData.bind(this)();
+        //this.$options.methods.loadYjsData.bind(this)();
+        //this.$options.methods.loadWlwData.bind(this)();
     },
     created: function() {
-        
+        /**
+         * 第一种方案（参考网易）http://www.codeceo.com/article/font-size-web-design.html
+         * [以iPhone4/5的设计稿为例js动态设置文档 rem 值]
+         * 6.4怎么来的，当然是根据设计稿的横向分辨率/100得到的
+         * 如果是750的设计稿，应该除以7.5
+         * 当deviceWidth大于640时，则物理分辨率大于1280（这就看设备的devicePixelRatio这个值了），应该去访问pc网站 所以加上这个判断if(deviceWidth > 640) deviceWidth = 640;
+         */
+        var deviceWidth = document.documentElement.clientWidth;
+        if (deviceWidth > 750) deviceWidth = 750;
+        document.documentElement.style.fontSize = deviceWidth / 7.2 + 'px';
         $('.module-right').each(function (index, element) {
             var itemNameWidth = 0;
             var itemDataWidth = 0;
@@ -172,110 +182,9 @@ var MainPage = {
             });
             _this.find('.item-data').width(itemDataWidth);
         });
-    }
-}
-// 云基
-// 信息发布平台
-var Infoplat = { template: '#infoplatpage' }
-// 系统配置平台
-var Config = { template: '#configpage'}
-// 基础组件平台
-var Coral = { template: '#coralpage'}
-// 全文检索平台
-var Search = { template: '#searchpage'}
-// 系统管理平台
-var Authsystem = { template: '#authsystempage'}
-// 工作流平台
-var Workflow = { template: '#workflowpage'}
-
-// 灵器
-// 电子文件通用浏览器
-var BrowserTool = {template: '#browsertoolpage'};
-// 电子文件封装工具
-var PackageTool = {template: '#packagetoolpage'};
-// 电子文件格式转换软件
-var ConvertTool = {template: '#converttoolpage'};
-// 电子文件检测软件
-var CheckTool = {template: '#checktoolpage'};
-// 电子文件离线浏览软件
-// var OfflineTool = {template: '#offlinetoolpage'};
-
-// 关于我们
-// 中心概述
-var CenterInfo = {template: '#centerinfopage'};
-// 组织沿革
-var OrganizeHistory = {template: '#organizehistorypage'};
-// 组织定位
-var OrganizeOrientation = {template: '#organizeorientationpage'};
-// 中心文化
-var CenterCulture = {template: '#centerculturepage'};
-
-// 2. 定义路由
-// 每个路由应该映射一个组件。 其中"component" 可以是
-// 通过 Vue.extend() 创建的组件构造器，
-// 或者，只是一个组件配置对象。
-// 我们晚点再讨论嵌套路由。
-var routes = [
-    { path: '/', component: MainPage },
-    { path: '/base/infoplat', component: Infoplat },
-    { path: '/base/config', component: Config },
-    { path: '/base/coral', component: Coral },
-    { path: '/base/search', component: Search },
-    { path: '/base/authsystem', component: Authsystem },
-    { path: '/base/workflow', component: Workflow },
-
-    { path: '/tool/browser', component: BrowserTool },
-    { path: '/tool/package', component: PackageTool },
-    { path: '/tool/convert', component: ConvertTool },
-    { path: '/tool/check', component: CheckTool },
-    // { path: '/tool/offline', component: OfflineTool },
-
-    { path: '/about/info', component: CenterInfo },
-    { path: '/about/history', component: OrganizeHistory },
-    { path: '/about/orientation', component: OrganizeOrientation },
-    { path: '/about/culture', component: CenterCulture }
-]
-
-// 3. 创建 router 实例，然后传 `routes` 配置
-// 你还可以传别的配置参数, 不过先这么简单着吧。
-var router = new VueRouter({
-    routes: routes
-})
-
-
-// 4. 创建和挂载根实例。
-// 记得要通过 router 配置参数注入路由，
-// 从而让整个应用都有路由功能
-var app = new Vue({
-    router: router,
-    data: function() {
-        return {
-            transitionComplete: false
-        }
-    },
-    computed: {
-        visible: function() {
-            return this.$route.path != '/';
-        }
-    },
-    methods: {
-        beforeEnter: function (el) {
-            // ...
-        },
-        afterEnter: function (el) {
-            if(this.$refs.topLink) {
-                var tl = this.$refs.topLink.$el;
-                tl.style.width = tl.getBoundingClientRect().height + "px";
-                tl.style.zIndex = 2;
-                this.transitionComplete = true;
-            }
-        },
-        beforeLeave: function (el) {
-            this.transitionComplete = false;
-        },
-        afterLeave: function (el) {
-            // ...
-        }
+		       
+		this.$options.methods.loadYjsData.bind(this)();
+        this.$options.methods.loadWlwData.bind(this)();
     }
 
-}).$mount('#app');
+});
