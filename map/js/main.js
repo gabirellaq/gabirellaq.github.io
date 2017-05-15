2a2e7ce87f93a58f3f8492ca1c1edcb6ebb0810b
qi.ying <qi.ying@cesgroup.com.cn>
2017-05-15 19:36:10 +0800

    字体

--- map/js/main.js
+++ map/js/main.js
@@ -7,7 +7,7 @@
  */
 var deviceWidth = document.documentElement.clientWidth;
 if(deviceWidth > 750) deviceWidth = 750;
-document.documentElement.style.fontSize = deviceWidth / 10.8 + 'px';
+document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
 
 	var $mapWrap = $('#mapWrap');
 	window.$mapWrap = $mapWrap;
